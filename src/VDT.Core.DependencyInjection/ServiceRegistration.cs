ab041b436b8da4b3cba7fb01024d96b1176e8215
Maikel Bos <maikel.bos0@gmail.com>
2023-06-05 11:02:35 +0200

    Add new service registration type and provider to replace service type provider

---
+++ src/VDT.Core.DependencyInjection/ServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace VDT.Core.DependencyInjection {
+    /// <summary>
+    /// Registration information for registering an implementation type
+    /// </summary>
+    public class ServiceRegistration {
+        /// <summary>
+        /// Create registration information for registering an implementation type
+        /// </summary>
+        /// <param name="serviceType">The type of the service to register the given implementation type as</param>
+        public ServiceRegistration(Type serviceType) {
+            ServiceType = serviceType;
+        }
+
+        /// <summary>
+        /// The type of the service to register the given implementation type as
+        /// </summary>
+        public Type ServiceType { get; set; }
+
+        /// <summary>
+        /// The <see cref="ServiceLifetime"/> for the service to register
+        /// </summary>
+        public ServiceLifetime? ServiceLifetime { get; set; }
+    }
+}
--- src/VDT.Core.DependencyInjection/ServiceRegistrationProvider.cs
+++ src/VDT.Core.DependencyInjection/ServiceRegistrationProvider.cs
@@ -6,8 +6,6 @@ namespace VDT.Core.DependencyInjection {
     /// Signature for methods that return service types for a given implementation type
     /// </summary>
     /// <param name="implementationType">The implementation type for which to return service types</param>
-    /// <returns>An enumerable of service types to register this implementation type for</returns>
-    /// <remarks>When using decorators, the service types must differ from the implementation type</remarks>
-    [Obsolete]
-    public delegate IEnumerable<Type> ServiceTypeProvider(Type implementationType);
+    /// <returns>An enumerable of registrations for the implementation type</returns>
+    public delegate IEnumerable<ServiceRegistration> ServiceRegistrationProvider(Type implementationType);
 }
