cb3cf04da17a60d1dfa03c8aa194766f3b736787
Maikel Bos <maikel.bos0@gmail.com>
2023-06-14 19:48:35 +0200

    Add missing attribute tests

---
+++ src/VDT.Core.DependencyInjection.Tests/Attributes/TransientAttributeTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VDT.Core.DependencyInjection.Attributes;
+using Xunit;
+
+namespace VDT.Core.DependencyInjection.Tests.Attributes {
+    public class TransientAttributeTests {
+        [Fact]
+        public void TransientAttribute_ServiceLifetime_Is_Transient() {
+            Assert.Equal(ServiceLifetime.Transient, new TransientAttribute().ServiceLifetime);
+        }
+    }
+}
