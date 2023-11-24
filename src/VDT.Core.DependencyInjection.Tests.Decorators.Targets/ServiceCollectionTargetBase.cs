54a1ea84146d2e0adb436199e189f15b1c219e73
Maikel Bos <maikel.bos0@gmail.com>
2023-11-24 14:32:18 +0100

    Cut unnecessary interface

--- src/VDT.Core.DependencyInjection.Tests.Decorators.Targets/ServiceCollectionTargetBase.cs
+++ src/VDT.Core.DependencyInjection.Tests.Decorators.Targets/ServiceCollectionTargetBase.cs
@@ -1,7 +1,7 @@
 ﻿using System.Threading.Tasks;
 
 namespace VDT.Core.DependencyInjection.Tests.Decorators.Targets {
-    public abstract class ServiceCollectionTargetBase : IServiceCollectionTargetImplementation {
+    public abstract class ServiceCollectionTargetBase : IServiceCollectionTarget {
         public abstract string Value { get; set; }
 
         public abstract Task<string> GetValue();
