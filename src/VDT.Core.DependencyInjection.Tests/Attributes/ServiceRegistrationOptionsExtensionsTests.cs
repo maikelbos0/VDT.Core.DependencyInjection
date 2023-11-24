36d87253a224a6a2e18e99f2507068403733f782
Maikel Bos <maikel.bos0@gmail.com>
2023-11-24 12:30:09 +0100

    Cut obsolete AddAttributeServiceTypeProviders

--- src/VDT.Core.DependencyInjection.Tests/Attributes/ServiceRegistrationOptionsExtensionsTests.cs
+++ src/VDT.Core.DependencyInjection.Tests/Attributes/ServiceRegistrationOptionsExtensionsTests.cs
@@ -6,66 +6,6 @@ using Xunit;
 namespace VDT.Core.DependencyInjection.Tests.Attributes {
     public class ServiceRegistrationOptionsExtensionsTests {
         [Fact]
-        public void AddAttributeServiceTypeProviders_ServiceTypeProviders_Find_ImplementationServiceAttributes() {
-            var services = new ServiceCollection();
-
-            services.AddServices(options => {
-                options.AddAttributeServiceTypeProviders();
-                options.Assemblies.Add(typeof(AttributeServiceImplementationTarget).Assembly);
-            });
-
-            var service = Assert.Single(services, s => s.ImplementationType == typeof(AttributeServiceImplementationTarget));
-            
-            Assert.Equal(typeof(IAttributeServiceImplementationTarget), service.ServiceType);
-            Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-        }
-
-        [Fact]
-        public void AddAttributeServiceTypeProviders_ServiceTypeProviders_Find_Interface_ServiceAttributes() {
-            var services = new ServiceCollection();
-
-            services.AddServices(options => {
-                options.AddAttributeServiceTypeProviders();
-                options.Assemblies.Add(typeof(IAttributeServiceInterfaceTarget).Assembly);
-            });
-
-            var service = Assert.Single(services, s => s.ServiceType == typeof(IAttributeServiceInterfaceTarget));
-
-            Assert.Equal(typeof(AttributeServiceInterfaceTarget), service.ImplementationType);
-            Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-        }
-
-        [Fact]
-        public void AddAttributeServiceTypeProviders_ServiceTypeProviders_Find_Base_Class_ServiceAttributes() {
-            var services = new ServiceCollection();
-
-            services.AddServices(options => {
-                options.AddAttributeServiceTypeProviders();
-                options.Assemblies.Add(typeof(AttributeServiceBaseClassTargetBase).Assembly);
-            });
-
-            var service = Assert.Single(services, s => s.ServiceType == typeof(AttributeServiceBaseClassTargetBase));
-
-            Assert.Equal(typeof(AttributeServiceBaseClassTarget), service.ImplementationType);
-            Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-        }
-
-        [Fact]
-        public void AddAttributeServiceTypeProviders_ServiceTypeProviders_Find_Implementation_Only_ServiceAttributes() {
-            var services = new ServiceCollection();
-
-            services.AddServices(options => {
-                options.AddAttributeServiceTypeProviders();
-                options.Assemblies.Add(typeof(AttributeServiceBaseClassTargetBase).Assembly);
-            });
-
-            var service = Assert.Single(services, s => s.ServiceType == typeof(AttributeServiceImplementationOnlyTarget));
-
-            Assert.Equal(typeof(AttributeServiceImplementationOnlyTarget), service.ImplementationType);
-            Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-        }
-
-        [Fact]
         public void AddAttributeServiceRegistrationProviders_ServiceRegistrationProviders_Find_Attributes() {
             var services = new ServiceCollection();
 
--- src/VDT.Core.DependencyInjection/Attributes/ServiceRegistrationOptionsExtensions.cs
+++ src/VDT.Core.DependencyInjection/Attributes/ServiceRegistrationOptionsExtensions.cs
@@ -8,46 +8,6 @@ namespace VDT.Core.DependencyInjection.Attributes {
     /// </summary>
     public static class ServiceRegistrationOptionsExtensions {
         /// <summary>
-        /// Add service type providers and service lifetime providers that find and use <see cref="TransientServiceAttribute"/>, <see cref="ScopedServiceAttribute"/>, <see cref="SingletonServiceAttribute"/>,
-        /// <see cref="TransientServiceImplementationAttribute"/>, <see cref="ScopedServiceImplementationAttribute"/> or <see cref="SingletonServiceImplementationAttribute"/> to register services
-        /// </summary>
-        /// <param name="options">The options for registering services</param>
-        /// <returns>A reference to this instance after the operation has completed</returns>
-        [Obsolete($"The separate delegates {nameof(ServiceTypeProvider)} and {nameof(ServiceLifetimeProvider)} have been deprecated; they have been replaced by the delegate {nameof(ServiceRegistrationProvider)} that returns both the service type and lifetime. This method will be removed in a future version.")]
-        public static ServiceRegistrationOptions AddAttributeServiceTypeProviders(this ServiceRegistrationOptions options) {
-            // Attributes on implementation types
-            options.AddServiceTypeProvider(
-                implementationType => implementationType.GetCustomAttributes(typeof(IServiceImplementationAttribute), false).Cast<IServiceImplementationAttribute>().Select(a => a.ServiceType),
-                (serviceType, implementationType) => implementationType.GetCustomAttributes(typeof(IServiceImplementationAttribute), false).Cast<IServiceImplementationAttribute>().FirstOrDefault(a => a.ServiceType == serviceType)?.ServiceLifetime
-            );
-
-            // Attributes on service interface types
-            options.AddServiceTypeProvider(
-                implementationType => implementationType.GetInterfaces().Where(serviceType => serviceType.GetCustomAttributes(typeof(IServiceAttribute), false).Cast<IServiceAttribute>().Any(a => a.ImplementationType == implementationType)),
-                (serviceType, implementationType) => serviceType.GetCustomAttributes(typeof(IServiceAttribute), false).Cast<IServiceAttribute>().FirstOrDefault(a => a.ImplementationType == implementationType)?.ServiceLifetime
-            );
-
-            // Attributes on service class types
-            options.AddServiceTypeProvider(
-                implementationType => {
-                    var currentServiceType = implementationType;
-                    var serviceTypes = new List<Type>();
-
-                    do {
-                        serviceTypes.Add(currentServiceType);
-                        currentServiceType = currentServiceType.BaseType;
-                    }
-                    while (currentServiceType != null);
-
-                    return serviceTypes.Where(serviceType => serviceType.GetCustomAttributes(typeof(IServiceAttribute), false).Any());
-                },
-                (serviceType, implementationType) => serviceType.GetCustomAttributes(typeof(IServiceAttribute), false).Cast<IServiceAttribute>().FirstOrDefault(a => a.ImplementationType == implementationType)?.ServiceLifetime
-            );
-
-            return options;
-        }
-
-        /// <summary>
         /// Add service registration providers that find and use <see cref="TransientServiceAttribute"/>, <see cref="ScopedServiceAttribute"/>, <see cref="SingletonServiceAttribute"/>,
         /// <see cref="TransientServiceImplementationAttribute"/>, <see cref="ScopedServiceImplementationAttribute"/> or <see cref="SingletonServiceImplementationAttribute"/> to register services
         /// </summary>
